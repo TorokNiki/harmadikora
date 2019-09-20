59fd39dd81213185ebf7ee2a73e7cb687fa71882
Leora <niki.torok@gmail.com>
2019-09-20 12:03:46 +0200

    Első lépések

---
+++ harmadikora20190920/harmadikora20190920/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harmadikora20190920
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
