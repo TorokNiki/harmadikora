a937dd1ebf465f348b4c07db2603c6b6dc85f1bd
Leora <niki.torok@gmail.com>
2019-09-20 12:15:36 +0200

    első feladat

--- harmadikora20190920/harmadikora20190920/Program.cs
+++ harmadikora20190920/harmadikora20190920/Program.cs
@@ -10,6 +10,27 @@ namespace harmadikora20190920
     {
         static void Main(string[] args)
         {
+            int legnagyobb = elso();
+            Console.WriteLine("A legnagyobb szám a: "+legnagyobb);
+        }
+        public static int elso()
+        {
+            int[] tomb = new int[10];
+            int max = 0;
+            Console.WriteLine("Most 10db egész számot kérünk öntöl.");
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                Console.Write("Kérem adja meg a {0}. számot: ",i+1);
+                tomb[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                if (max<tomb[i])
+                {
+                    max = tomb[i];
+                }
+            }
+            return max;
         }
     }
 }
